d8a6571c5e6da94603e4c55a371c226a88b9f012
chhw130 <527coco@naver.com>
2025-04-30 20:09:50 +0900

    feat : reduce test

--- ep04/src/reduce.test.js
+++ ep04/src/reduce.test.js
@@ -6,13 +6,20 @@ describe('reduce method', () => {
     const numbers = [1, 2, 3, 4, 5];
 
     // TODO: do something here
-    const sum = 0;
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
     expect(sum).toBe(15);
   });
 
   it('groups by genre', () => {
     // TODO: do something with `shows` here
-    const groupedShows = [];
+    const groupedShows = shows.reduce((acc, cur) => {
+      if (acc[cur.genre]) {
+        acc[cur.genre].push(cur.title);
+        return acc;
+      }
+      acc[cur.genre] = [cur.title];
+      return acc;
+    }, {});
     expect(groupedShows).toEqual({
       Comedy: ["Don't Look Up"],
       Drama: ['Stranger Things', 'Our Blues', 'Inventing Anna'],
@@ -23,7 +30,19 @@ describe('reduce method', () => {
 
   it('groups by key (2)', () => {
     // TODO: do something with `shows` here
-    const groupedShows = [];
+    const groupedShows = shows.reduce((acc, cur) => {
+      const idx = acc.findIndex((result) => result.genre === cur.genre);
+
+      if (idx === -1) {
+        acc.push({
+          genre: cur.genre,
+          titles: [cur.title],
+        });
+        return acc;
+      }
+      acc[idx].titles.push(cur.title);
+      return acc;
+    }, []);
     expect(groupedShows).toEqual([
       {
         genre: 'Drama',
@@ -52,13 +71,17 @@ describe('reduce method', () => {
     ];
 
     // TODO: do something here
-    const flatArray = [];
+    const flatArray = nestedArray.reduce((acc, cur) => acc.concat(...cur), []);
     expect(flatArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
   it('extracts writer names', () => {
     // TODO: do something with `shows` here
-    const writerNames = [];
+    const writerNames = shows.reduce((acc, show) => {
+      const writers = show.writers;
+      acc = [...new Set(acc.concat(...writers))];
+      return acc;
+    }, []);
     expect(writerNames).toEqual([
       'Matt Duffer',
       'Ross Duffer',
